d963871e0ff07d8286db27a464893a1a79372f1a
President Not Sure <144360653+president-not-sure@users.noreply.github.com>
2024-10-16 09:42:18 -0400

    Revert "E501 line too long (80 > 79 characters)"
    This reverts commit ba3d300fdd1de30dc3ca072abea94f98bbf904ed.

--- tests/test_lppn.py
+++ tests/test_lppn.py
@@ -40,9 +40,8 @@ class TestLppnGetMethod(unittest.TestCase):
         self.assertIsInstance(type(patch), type(int))
         self.assertEqual(patch, self.latest_patch)
 
-    @mock.patch("requests.sessions.Session.get")
+    @mock.patch("requests.sessions.Session.get", return_value=MockResponse(200))
     def test_valid_int_input(self, mock_session_get):
-        mock.return_value = MockResponse(200)
         patch = lppn.get(self.major, self.minor)
         mock_session_get.assert_called_once()
         self.assertIsInstance(type(patch), type(int))
